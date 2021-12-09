2a0e8b4e9c3a63f8dffc7a140fa17f2c91adcb8f
NicoCastillo <castillonicolas1996@gmail.com>
2021-12-09 11:29:18 -0300

    9/12

--- src/controllers/controllerPosteos.js
+++ src/controllers/controllerPosteos.js
@@ -1,43 +1,84 @@
-import Posts from '../models/posts.js'
-let posts = [ ]
+import Post from '../models/posts.js'
 
 export const viewInicio = async (req,res) => {
-  res.status(200).render('inicio',{posts:posts}) 
+  try {
+    let post =  await Post.find({}).lean()
+      res.status(200).render('inicio',{posts:post})
+    } 
+    catch (e) { console.log(e) }
 }
 
 export const viewUser= async (req,res)=>{
-  
-    res.status(200).render('usuario',{posteos:posts})
+  try {
+  let post =  await Post.find({}).lean()
+    res.status(200).render('usuario',{posteos:post})
+  } 
+  catch (e) { console.log(e) }
+   
   }
   
 export const createPost = async (req,res)=>{
   try {
     const post= new Post(req.body)
     await post.save()
+    res.status(200).redirect('/usuario')
     
   } 
   catch (e) { console.log(e) }
-  res.status(200).redirect('/usuario')
-}
-
-export const delPost =(req,res) =>{
-  console.log(req.body)
-  posts = posts.filter(element => element.id != req.body.id)
-  res.status(200).redirect('/usuario')
   
 }
 
-export const updatePost = (req,res) => {
-  let post = posts.find(element => element.id == req.body.id)
-  if(req.body.title) post.title = req.body.title
-  if(req.body.desc) post.desc = req.body.desc
-  res.status(200).redirect('/usuario')
+export const delPost = async (req,res) =>{
+  try {
+    const postfound = await Post.find({_id:req.body._id}).lean()
+       if ((Object.entries(postfound).length === 0)) {
+         return res.status(200).render("nofound",{message:"no se encontro el Producto"})
+       }
+       await Post.deleteOne({ _id: req.body._id }) 
+       res.status(200).redirect('/usuario')
+} 
+ catch (e) { console.log(e) }
+ 
+  
 }
 
-export const createComentario = (req,res)=>{
-  let post = posts.find(element => element.id == req.body.idProduct)
-  console.log(post)
-  post.comentarios.push(req.body.comment)
-  console.log(posts)
+export const updatePost = async (req,res) => {
+
+  try {
+    const postfound = await Post.find({_id:req.body._id}).lean()
+        if ((Object.entries(postfound).length === 0)) {
+          return res.status(200).render("nofound",{message:"no se encontro el Producto"})
+        }
+    await Post.findOneAndUpdate(
+      { _id: req.body._id },
+      { $set: req.body },
+      { new: true }
+    )  
+    res.status(200).redirect('/usuario')
+  } 
+  catch (e) { console.log(e) }
+ 
+}
+
+export const createComentario = async (req,res)=>{
+  try {
+
+    const postfound = await Post.find({_id:req.body.idProduct}).lean()
+        if ((Object.entries(postfound).length === 0)) {
+          return res.status(200).render("nofound",{message:"no se encontro el Producto"})
+        }  
+
+    let modificacion = {}
+    modificacion.comentario = postfound[0].comentario
+    modificacion.comentario.push(req.body.comment)
+
+    await Post.findOneAndUpdate(
+      { _id: req.body.idProduct },
+      { $set: modificacion },
+      { new: true }
+    )  
+    res.status(200).redirect('/usuario')
+  } 
+  catch (e) { console.log(e) }
 
 }
--- src/models/favoritos.js
+++ src/models/favoritos.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
-const postsCollection = 'posts'
-const postsSchema = new mongoose.Schema({
+const favoritosCollection = 'favoritos'
+const favoritosSchema = new mongoose.Schema({
   title: { type: String, require: true },
   desc: { type: String, require: true },
   comentario:{type:Array ,require:true}
 })
 
-export default mongoose.model(postsCollection, postsSchema)
+export default mongoose.model(favoritosCollection,favoritosSchema)
